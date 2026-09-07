491fe4bc875883be152a47e7c6d5d3ad10bb97c5
stoilsteve-hub <stoil.steve@me.com>
2026-09-07 20:51:32 +0200

    Added logout disable to SecurityConfig

--- booking-service/src/main/java/com/example/bookingapp/config/SecurityConfig.java
+++ booking-service/src/main/java/com/example/bookingapp/config/SecurityConfig.java
@@ -46,6 +46,7 @@ public class SecurityConfig {
                         SessionCreationPolicy.IF_REQUIRED))
                 .addFilterBefore(jwtFilter,
                         UsernamePasswordAuthenticationFilter.class)
+                .logout(AbstractHttpConfigurer::disable)
                 .build();
     }
 }
